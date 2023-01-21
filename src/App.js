c7b35597cfe3e9d5a50206d4e54dd479d9593266
Deepak <dsahni633@gmail.com>
2023-01-21 09:00:15 +0530

    Added About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import About from "./About";
 import Navbar from "./components/Navbar"
-import TextForm from "./components/TextForm"
+// import TextForm from "./components/TextForm"
 function App() {
   return (
     <>
       <Navbar title="TextUtils" about="About TextUtils" />
-      <TextForm heading= "Enter text to analyze below"/>
+      {/* <TextForm heading="Enter text to analyze below" /> */}
+      <About/>
     </>
   );
 }
