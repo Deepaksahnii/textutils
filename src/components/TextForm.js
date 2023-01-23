84f7c2a4a96c7fc98a42f99130d3ef77a7184ef1
Deepak <dsahni633@gmail.com>
2023-01-23 17:45:20 +0530

    Some improvements related to dark mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -33,10 +33,10 @@ export default function TextForm(props) {
     }
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
           <h1>{props.heading}</h1>
-    <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8" placeholder="Enter text here..."></textarea>
+    <div className="mb-3" style={{color: props.mode==='dark'?'white':'black'}}>
+  <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8" placeholder="Enter text here..." style={{backgroundColor: props.mode==='dark'? '#748aa8': 'white', color:props.mode=== 'dark'?'white': 'black'}}></textarea>
           </div>
           <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to upper Case</button>
           <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lower Case</button>
@@ -45,12 +45,12 @@ export default function TextForm(props) {
           <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
           <button className="btn btn-primary mx-1" onClick={handleEmail}>Extract emails</button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} words and {text.length} characters.</p>
          <p>{0.008 * text.split(" ").length} minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in the above text box to preview it here"}</p>
       </div>
       </>
   )
